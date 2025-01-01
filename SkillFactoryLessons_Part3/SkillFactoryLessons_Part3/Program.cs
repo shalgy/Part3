e734174385ce687afe5cabbfa293fc633a278bc4
shalgy <lexashalgy1978@gmail.com>
2025-01-01 19:52:43 +0300

    Фиксация промежуточного задание на работу с переменными, перечислениями, конвертацию типов

---
+++ SkillFactoryLessons_Part3/SkillFactoryLessons_Part3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace SkillFactoryLessons_Part3
+{
+    public enum FavoriteDay
+    {
+        понедельник = 1,
+        вторник,
+        среда,
+        четверг,
+        пятница,
+        суббота,
+        воскресенье
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Введите Ваше имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите Ваш возраст: ");
+            byte age = checked((byte)int.Parse(Console.ReadLine()));
+            Console.Write("Введите номер вашего любимого дня недели: ");
+            byte day = (byte)int.Parse(Console.ReadLine());
+            Console.WriteLine("Ваше имя {0}, Ваш возраст {1}, Ваш любимый день: {2}", name, age, (FavoriteDay)day);
+            Console.ReadKey();
+        }
+    }
+}
